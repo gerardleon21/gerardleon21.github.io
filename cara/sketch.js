84fd9a2715ed1b61be7ddf36a558139ef8116f79
gerardleon21 <gleon4@iespompeufabra.cat>
2024-09-30 13:27:27 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -12,17 +12,17 @@ function draw() {
   console.log(vulls)
   console.log(vol)
   background(127);
-  fill(227,192,139);
+  fill(255,0,0);
   ellipse(300,200,350,300);
-  fill(19,293,29);
+  fill(255,255,9);
   ellipse(250,150,30,50);
-  fill(200,42,14);
+  fill(20,42,14);
   ellipse(250,150,10*sin(frameCount*0.1)+15,15);
   fill(190,29,389);
   ellipse(350,150,30,50);
-  fill(200,42,14);
+  fill(190,29,389);
   ellipse(3700,170,20*cos(frameCount*0.1)+15,15);
-  fill(200,150,15);
+  fill(0,0,0);
   arc(300,250,200,3+vboca,0,PI,CHORD);
  
 }
