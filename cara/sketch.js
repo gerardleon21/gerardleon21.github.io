0750f53bbe2d2d1b027efb0cf4fe87705530248e
gerardleon21 <gleon4@iespompeufabra.cat>
2024-09-30 13:21:07 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -1,34 +1,28 @@
-function setup() {
-  createCanvas(480, 120);
+function setup() 
+{
+  createCanvas(600,400);
+  mic=new p5.AudioIn();
+  mic.start();
 }
 
-function draw(){
-  background(204);
-  //OcellEsquerra
-  beginShape();
-  vertex(50,120);
-  vertex(100,90);
-  vertex(110,60);
-  vertex(80,20);
-  vertex(210,60);
-  vertex(160,80);
-  vertex(200,90);
-  vertex(140,100);
-  vertex(130,120);
-  endShape();
-  fill(0);
-  ellipse(155,60,8,8);
-  //Ocell dreta
-  beginShape();
-  vertex(370,130);
-  vertex(360,90);
-  vertex(290,80);
-  vertex(80,20);
-  vertex(210,60);
-  vertex(160,80);
-  vertex(200,90);
-  vertex(140,100);
-  vertex(130,120);
-  endShape();
-  fill(0);
-  ellipse(155,60,8,8);
+function draw() {
+  let vol= mic.getLevel();
+  let vulls= map(vol,0,1,0,100);
+  let vboca= map(vol,0,1,0,300);
+  console.log(vulls)
+  console.log(vol)
+  background(127);
+  fill(227,192,139);
+  ellipse(300,200,350,300);
+  fill(19,293,29);
+  ellipse(250,150,30,50);
+  fill(200,42,14);
+  ellipse(250,150,10*sin(frameCount*0.1)+15,15);
+  fill(190,29,389);
+  ellipse(350,150,30,50);
+  fill(200,42,14);
+  ellipse(3700,170,20*cos(frameCount*0.1)+15,15);
+  fill(200,150,15);
+  arc(300,250,200,3+vboca,0,PI,CHORD);
+ 
+}
