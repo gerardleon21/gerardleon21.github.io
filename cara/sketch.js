b2e27968a63e52544ab0e0bee7ffafb8d68f4e90
gerardleon21 <gleon4@iespompeufabra.cat>
2024-10-27 16:20:18 +0100

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -1,28 +1,42 @@
-function setup() 
-{
-  createCanvas(600,400);
-  mic=new p5.AudioIn();
+let mic;
+
+function setup() {
+  createCanvas(600, 400);
+  mic = new p5.AudioIn();
   mic.start();
 }
 
 function draw() {
-  let vol= mic.getLevel();
-  let vulls= map(vol,0,1,0,100);
-  let vboca= map(vol,0,1,0,300);
-  console.log(vulls)
-  console.log(vol)
+  let vol = mic.getLevel();
+  let ullsAmp = map(vol, 0, 1, 0, 15); 
+  let bocaAmp = map(vol, 0, 1, 10, 100); 
   background(127);
-  fill(255,0,0);
-  ellipse(300,200,350,300);
-  fill(255,255,9);
-  ellipse(250,150,30,50);
-  fill(20,42,14);
-  ellipse(250,150,10*sin(frameCount*0.1)+15,15);
-  fill(190,29,389);
-  ellipse(350,150,30,50);
-  fill(190,29,389);
-  ellipse(3700,170,20*cos(frameCount*0.1)+15,15);
-  fill(0,0,0);
-  arc(300,250,200,3+vboca,0,PI,CHORD);
- 
+
+  // Cara
+  fill(255, 204, 153);
+  noStroke();
+  ellipse(300, 200, 350, 300);
+
+  // Ull esquerre
+  fill(255);
+  ellipse(230, 150, 50, 70); 
+  fill(20, 42, 14);
+  ellipse(230 + ullsAmp, 150, 20, 30); // Pupil·la
+
+  // Ull dret
+  fill(255);
+  ellipse(370, 150, 50, 70); // 
+  fill(20, 42, 14);
+  ellipse(370 + ullsAmp, 150, 20, 30); // Pupil·la 
+
+  // Boca
+  fill(0);
+  arc(300, 250, 200, bocaAmp, 0, PI, CHORD);
+
+  // Cellas
+  stroke(0);
+  strokeWeight(4);
+  line(200, 120, 260, 130); 
+  line(340, 130, 390, 120); 
 }
+
